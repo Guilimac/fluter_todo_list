424b7ebe2c70a44373b233cef23eab63e3d6f7d8
Guilherme de Lima Conceição <guilherme.lima@ed.company>
2020-12-22 09:31:12 -0300

    remoção de item com opção desfazer

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,10 @@ class _HomeState extends State<Home> {
   final _todoController = TextEditingController();
   List _todoList = [];
 
+  Map<String, dynamic> _lastRemoved;
+
+  int _lastRemovedPos;
+
 
   @override
   void initState() {
@@ -110,7 +114,29 @@ class _HomeState extends State<Home> {
               _saveData();
             });
           },
-        )
+        ),
+        onDismissed: (direction){
+        setState(() {
+          _lastRemoved = Map.from(_todoList[index]);
+          _lastRemovedPos = index;
+          _todoList.removeAt(index);
+
+          _saveData();
+
+          final snack = SnackBar(
+              content: Text("Tarefa ${_lastRemoved["title"]} removida!"),
+            action: SnackBarAction(label: "Desfazer", onPressed: (){
+              setState(() {
+                _todoList.insert(_lastRemovedPos, _lastRemoved);
+                _saveData();
+              });
+            },
+            ),
+            duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
