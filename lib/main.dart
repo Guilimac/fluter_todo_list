a0bd42347158a2b16a4c146dbff4f3889a49d3ba
Guilherme de Lima Conceição <guilherme.lima@ed.company>
2020-12-21 21:31:52 -0300

    criação de leitura e gravação de arquivo no storage e campo text e botão de add itens

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import "package:flutter/material.dart";
+import "package:path_provider/path_provider.dart";
+import "dart:async";
+import "dart:io";
+import "dart:convert";
+
+void main(){
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List _todoList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
+            child: Row(
+              children: [
+                Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          labelText: "Nova Tarefa",
+                          labelStyle: TextStyle(color: Colors.blueAccent)
+                      ),
+                    ),
+                ),
+                RaisedButton(
+                  onPressed: (){},
+                  color: Colors.blueAccent,
+                  child: Text("ADD"),
+                  textColor: Colors.white,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<File> _getFile() async{
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_todoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async{
+    try{
+      final file = await _getFile();
+
+      return file.readAsString();
+    }catch(e){
+      return null;
+    }
+  }
+}
+
+
